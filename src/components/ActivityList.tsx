e837d17e3347322d7c526a2281d80d1ae2f453e4
Chema <jomaroru7@gmail.com>
2024-11-12 12:51:51 +0100

    Added modify activity button

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -2,6 +2,8 @@ import { useMemo } from "react"
 import { Activity } from "../types"
 import { categories } from "../data/categories"
 
+import { PencilSquareIcon } from "@heroicons/react/24/outline"
+
 type ActivityListProps = {
     activities: Activity[]
 }
@@ -27,8 +29,12 @@ export default function ActivityList({ activities }: ActivityListProps) {
                             <span>Calorías</span>
                         </p>
                     </div>
-                    <div>
-
+                    <div className="flex gap-5 items-center">
+                        <button>
+                            <PencilSquareIcon
+                                className="h-8 w-8 text-gray-800"
+                            />
+                        </button>
                     </div>
                 </div>
             ))}
